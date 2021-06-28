8eeaa133295967b5e5526b508c86d27c010edfb5
mchandrechynto <andrechynto@webmail.umm.ac.id>
2021-06-28 13:31:21 +0700

    initial release

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" href="{{ asset('img/icon.png') }}" sizes="16x16" type="image/png">
     <title>UICORE</title>
